44b13c99f4bcf251a0e7345769cce637d8e5ddc2
Todd Schueren <19.todd.schueren@ksd.org>
2019-02-20 10:17:05 -0800

    worked on player base code

---
+++ groupMobileGame/Assets/Scripts/UIScripts/lose.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lose : MonoBehaviour
+{
+   
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("PlayerBase").GetComponent<playerBaseHp>().Hp <= 0)
+        {
+            GetComponent<Canvas>().enabled = true;
+        }
+    }
+}
---
+++ groupMobileGame/Assets/Scripts/playerBaseHp.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBaseHp : MonoBehaviour
+{
+    public int Hp = 100;
+    
+ 
+}
