b6f5a097e589c95f9b68d47fe717da89f62c13a5
Todd Schueren <19.todd.schueren@ksd.org>
2019-03-05 09:03:07 -0800

    level and difficulty Select

---
+++ groupMobileGame/Assets/Scripts/UIScripts/difficultySelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class difficultySelect : MonoBehaviour
+{
+    public void Normal()
+    {
+        SceneManager.LoadScene("levelSelectN");
+    }
+
+    public void Hard()
+    {
+        SceneManager.LoadScene("levelSelectH");
+    }
+
+    public void Lunatic()
+    {
+        SceneManager.LoadScene("levelSelectL");
+    }
+}
